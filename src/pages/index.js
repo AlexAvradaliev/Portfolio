cedca2cc7e478ebc7b6fba57e19481ddc75c391b
aleksandarAvr <avradaliev@gmail.com>
2022-10-08 16:25:06 +0300

    change function with arrow

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
     <></>
   )
-}
+};
+
+export default Home;
